c23e6814b4b8304d392041e2cb6e1878d3462356
menandro matos <menandro1968@gmail.com>
2025-10-26 22:06:54 -0400

    Corregir import de db - usar destructuring const { db }

--- taxi-backend/routes/drivers.js
+++ taxi-backend/routes/drivers.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const db = require('../config/database');
+const { db } = require('../config/database');
 
 // REGISTRO DE CONDUCTOR
 router.post('/register', async (req, res) => {
